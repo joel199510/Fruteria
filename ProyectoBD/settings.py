6b9d7d591c2e830d34ea550838633c8f2ae08f3a
joelvergara1 <joel.vergara@alumnos.duoc.cl>
2020-03-17 19:26:23 -0300

    bd

--- ProyectoBD/settings.py
+++ ProyectoBD/settings.py
@@ -121,8 +121,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
